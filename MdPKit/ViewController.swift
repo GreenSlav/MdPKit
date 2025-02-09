772f1118cd042797f527ba53202b8dd72083d426
Viacheslav <sromanov2608@gmail.com>
2025-02-09 04:00:34 +0300

    Initial Commit

---
+++ MdPKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MdPKit
+//
+//  Created by green_sl on 9.2.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
